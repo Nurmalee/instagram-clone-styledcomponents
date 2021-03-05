c2f6dbb3fd9d4b6eb56111d1b9ee0cecb0a4393f
Lawal Nurudeen <lawalnurudeenfocus@gmail.com>
2021-03-05 23:50:32 +0100

    STYLED-STORIES BAR/NAV - stories rerendered from static array - code cleanedup

--- src/components/Stories.js
+++ src/components/Stories.js
@@ -1,61 +1,24 @@
 import React from 'react'
 import styled from 'styled-components'
 import { Avatar } from '@material-ui/core';
+import { storiesData } from '.././data/appUiData'
 
 const Stories = () => {
     return (
         <StoriesContainer>
-
-            <StoryContainer>
-                <AvatarBg>
-                    <StoriesAvatar src="https://firebasestorage.googleapis.com/v0/b/linkedin-webapp-clone.appspot.com/o/images%2FLee%20Logo.png?alt=media&token=1927abc3-7155-4a13-9bb5-fb4c245c2e14" />
-                </AvatarBg>
-                <p>The Lee</p>
-            </StoryContainer>
-          
-            <StoryContainer>
-                <AvatarBg>
-                    <StoriesAvatar src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_nWUWzq7si2Z0V91ziLuth57egprQSbQcXw&usqp=CAU" />
-                </AvatarBg>
-                <p>Rihanna</p>
-            </StoryContainer>
-
-            <StoryContainer>
-                <AvatarBg>
-                    <StoriesAvatar src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQCWtt8VduiqgHuvY9tY2pQpPguYLrQRClbnA&usqp=CAU" />
-                </AvatarBg>
-                <p>Cristiano</p>
-            </StoryContainer>
-           
-            <StoryContainer>
-                <AvatarBg>
-                    <StoriesAvatar src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTopMx7dcoqspKyWtme4twKvHViAQg9YBkj-g&usqp=CAU" />
-                </AvatarBg>
-                <p>Nicki Minaj</p>
-            </StoryContainer>
-
-            <StoryContainer>
-                <AvatarBg>
-                    <StoriesAvatar src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-lIsxlQmxODlpTRxUy2fJmi14FwcsJbhbCg&usqp=CAU" />
-                </AvatarBg>
-                <p>Brianaamoore</p>
-            </StoryContainer>
-
-           
-            <StoryContainer>
-                <AvatarBg>
-                    <StoriesAvatar src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7wxzgd28yMN8y6RQXm4axmp4QIiuTpr2O9A&usqp=CAU" />
-                </AvatarBg>
-                <p>Marques Brownlee</p>
-            </StoryContainer>
-           
-            <StoryContainer>
-                <AvatarBg>
-                    <StoriesAvatar src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjYqVOz_VI8N7z0EIoCE--obgFS9rhee3rjw&usqp=CAU" />
-                </AvatarBg>
-                <p>Israel Adesanya</p>
-            </StoryContainer>
-            
+            {
+                storiesData.map(story => {
+                    const {name, imageSrc} = story;
+                    return (
+                        <StoryContainer>
+                            <AvatarBg>
+                                <StoriesAvatar src={imageSrc} />
+                            </AvatarBg>
+                            <p> {name} </p>
+                        </StoryContainer>
+                    )
+                })
+            }
         </StoriesContainer>
     )
 }
---
+++ src/data/appUiData.js
@@ -0,0 +1,31 @@
+// import React from 'react'
+export const storiesData = [
+    {
+        name: "The Lee",
+        imageSrc: "https://firebasestorage.googleapis.com/v0/b/linkedin-webapp-clone.appspot.com/o/images%2FLee%20Logo.png?alt=media&token=1927abc3-7155-4a13-9bb5-fb4c245c2e14",
+    },
+    {
+        name: "Rihanna",
+        imageSrc: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_nWUWzq7si2Z0V91ziLuth57egprQSbQcXw&usqp=CAU",
+    },
+    {
+        name: "Cristiano",
+        imageSrc: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQCWtt8VduiqgHuvY9tY2pQpPguYLrQRClbnA&usqp=CAU",
+    },
+    {
+        name: "Nicki Minaj",
+        imageSrc: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTopMx7dcoqspKyWtme4twKvHViAQg9YBkj-g&usqp=CAU",
+    },
+    {
+        name: "Brianaamoore",
+        imageSrc: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-lIsxlQmxODlpTRxUy2fJmi14FwcsJbhbCg&usqp=CAU",
+    },
+    {
+        name: "Marques Brownlee",
+         imageSrc: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7wxzgd28yMN8y6RQXm4axmp4QIiuTpr2O9A&usqp=CAU",
+    },
+    {
+        name: "Israel Adesanya",
+        imageSrc: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjYqVOz_VI8N7z0EIoCE--obgFS9rhee3rjw&usqp=CAU",
+    },
+]
