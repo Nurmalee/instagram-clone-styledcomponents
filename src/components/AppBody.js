31a9d1688251452b25befafd145582dbf13e6cfc
Lawal Nurudeen <lawalnurudeenfocus@gmail.com>
2021-03-06 01:23:37 +0100

    CREATED-USER-PROFILE-CONTAINER - little stuff

--- src/components/AppBody.js
+++ src/components/AppBody.js
@@ -2,12 +2,20 @@ import React from 'react'
 import Post from './Post'
 import Stories from './Stories';
 import styled from 'styled-components'
+import Profile from './Profile';
 
 const AppBody = () => {
     return (
         <AppBodyContainer>
-            <Stories />
-            <Post />        
+            <MainBody>
+                <Stories />
+                <Post />  
+            </MainBody>
+
+            <UserProfile>
+                <Profile />
+            </UserProfile>
+               
         </AppBodyContainer>
     )
 }
@@ -15,6 +23,28 @@ const AppBody = () => {
 export default AppBody
 
 const AppBodyContainer = styled.section`
+    border: 2px solid;
+    display: flex;
+    margin: 0 auto;
+    max-width: 900px;
+
+
+`
+
+const MainBody = styled.div`
+
+flex: 0.7
+border: 1px solid;
+ 
+
+`
+
+const UserProfile = styled.div`
+
+flex: 0.3;
+border: 1px solid;
+background-color: green;
+   
 
 `
 
---
+++ src/components/Profile.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Profile = () => {
+    return (
+        <ProfileContainer>
+            
+        </ProfileContainer>
+    )
+}
+
+export default Profile
+
+const ProfileContainer = styled.section`
+    height: 500px;
+ 
+
+`
