0e31cb93ec04f9d1541b567fe0404177e256486e
Lawal Nurudeen <lawalnurudeenfocus@gmail.com>
2021-03-09 00:30:35 +0100

    POST INPUT FORM - customized file input button and activated file selection ability

--- src/components/PostInput.js
+++ src/components/PostInput.js
@@ -2,16 +2,33 @@ import React, { useState } from 'react'
 import styled from 'styled-components'
 import { MdCloudUpload } from 'react-icons/md';
 import { RiSendPlaneLine } from 'react-icons/ri';
+import { instagramCloneDb, firebaseServerTime } from '.././config/firebaseConfig'
 
 
 const PostInput = ({showPostInput, setShowPostInput}) => {
 
     const [textInput, setTextInput] = useState("")
+    const [imageFile, setImageFile] = useState(null)
 
     const handlePostSubmit = (e) => {
         e.preventDefault()
-        // setShowPostInput(false)
         
+        if(textInput){
+            instagramCloneDb.collection("posts").add({
+                name: "Nurmalee",
+                pictureUrl: null,
+                text: textInput,
+                timestamp: firebaseServerTime,
+            })
+        }
+        setTextInput("")
+        // setShowPostInput(false)
+    }
+
+    const attachImageFile = (e) => {
+        if(imageFile){
+            setImageFile(e.target.files[0])
+        }
     }
 
     if(showPostInput){
@@ -22,7 +39,11 @@ const PostInput = ({showPostInput, setShowPostInput}) => {
                     <textarea rows="10" placeholder="Say something about this post or just upload a picture..." value={textInput} onChange={(e) => setTextInput(e.target.value)} />
 
                     <div>
-                        <button> <MdCloudUpload style={{height: "20px", width: "20px", marginRight: "10px"}}/> upload image </button>
+                        <label htmlFor='file'>
+                            <MdCloudUpload style={{height: "20px", width: "20px", marginRight: "10px"}}/>
+                            <p>upload image</p>
+                            <input type='file' id='file' onChange={attachImageFile} />
+                        </label>
                         <button type="submit" > <RiSendPlaneLine  style={{height: "20px", width: "20px", marginRight: "10px"}} /> send post </button>
                     </div>
                 </form>
@@ -69,7 +90,7 @@ const PostFormContainer = styled.div`
         display: flex;
     }
 
-    > form > div > button {
+    > form > div > button, form > div > label {
         flex: 1;
         padding: 10px 0;
         text-transform: capitalize;
@@ -81,5 +102,27 @@ const PostFormContainer = styled.div`
         font-weight: 900;
         /* border-radius: 5px; */
     }
+
+    >  form > div > label {
+        padding: 0;
+        background-color: brown;
+        color: white;
+        border: 1px solid;
+        cursor: pointer;
+        > p {
+            font-size: 14px;
+        }
+    }
+
+    > form > div > label > input {
+        width: 0.1px;
+	    height: 0.1px;
+	    opacity: 0;
+	    overflow: hidden;
+	    position: absolute;
+	    z-index: -1;
+    }
+
+  
 
 `
