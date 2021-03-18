7542c7b9ac5cb3939be92d37de0da5e5f291439d
Lawal Nurudeen <lawalnurudeenfocus@gmail.com>
2021-03-18 19:55:20 +0100

    IMPORTING STYLED STYLES - testing export and import styled styles

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,9 @@
 import React, {useState} from 'react'
-import styled from 'styled-components'
+// import styled from 'styled-components'
 import { Link, useHistory } from 'react-router-dom'
 import { useUserAuth } from '.././contextAPI/userContext'
+
+import { LoginContainer, LoginHeader, LoginForm, LoginBottom } from './LoginStyles'
 
 const Login = () => {
 
@@ -57,87 +59,87 @@ const Login = () => {
 
 export default Login
 
-const LoginContainer = styled.div`
-    display: grid;
-    margin: 70px auto;
-    padding: 20px 10px;
-    text-align:center;
-    max-width: 400px;
-    font-size: 14px;
+// const LoginContainer = styled.div`
+//     display: grid;
+//     margin: 70px auto;
+//     padding: 20px 10px;
+//     text-align:center;
+//     max-width: 400px;
+//     font-size: 14px;
 
-    > p {
-        cursor: pointer;
-        padding: 10px;
-        margin-bottom: 15px;
-        font-weight: 600;
-        color: red;
-        background-color: rgba(200, 0, 0, 0.2);
-    }
-`
+//     > p {
+//         cursor: pointer;
+//         padding: 10px;
+//         margin-bottom: 15px;
+//         font-weight: 600;
+//         color: red;
+//         background-color: rgba(200, 0, 0, 0.2);
+//     }
+// `
 
-const LoginHeader = styled.div`
-    margin-bottom: 10px;
+// const LoginHeader = styled.div`
+//     margin-bottom: 10px;
 
-    > img {
-        height: 45px;
-        object-fit: contain;
-        cursor: pointer;
-    }
-`
+//     > img {
+//         height: 45px;
+//         object-fit: contain;
+//         cursor: pointer;
+//     }
+// `
 
-const LoginForm = styled.form`
-    display: flex;
-    flex-direction: column;
+// const LoginForm = styled.form`
+//     display: flex;
+//     flex-direction: column;
 
-    > input {
-        padding: 12px 10px;
-        width: 100%;
-        margin-bottom: 15px;
-        font-size: 14px;
-        outline: none;
-        border: none;
-        border-radius: 2px;
-        box-shadow: 0 0 3px black;
+//     > input {
+//         padding: 12px 10px;
+//         width: 100%;
+//         margin-bottom: 15px;
+//         font-size: 14px;
+//         outline: none;
+//         border: none;
+//         border-radius: 2px;
+//         box-shadow: 0 0 3px black;
 
-        :focus {
-            outline: 1px solid;
-        }
-    }
+//         :focus {
+//             outline: 1px solid;
+//         }
+//     }
 
-    > button {
-        padding: 12px;
-        width: 100%;
-        font-size: 12px;
-        font-weight: 700;
-        letter-spacing: 1px;
-        text-transform: uppercase;
-        outline: none;
-        border: 1px solid black;
-        border-radius: 2px;
-        background-color: #333;
-        color: grey;
-        transition: 1500ms;
+//     > button {
+//         padding: 12px;
+//         width: 100%;
+//         font-size: 12px;
+//         font-weight: 700;
+//         letter-spacing: 1px;
+//         text-transform: uppercase;
+//         outline: none;
+//         border: 1px solid black;
+//         border-radius: 2px;
+//         background-color: #333;
+//         color: grey;
+//         transition: 1500ms;
 
-        :hover {
-            background-color: black;
-            color: white;
-        }
-    }
-`
+//         :hover {
+//             background-color: black;
+//             color: white;
+//         }
+//     }
+// `
 
-const LoginBottom = styled.div`
-    border-top: 1px solid #ccc;
-    margin-top: 20px;
-    padding: 10px 0;
+// const LoginBottom = styled.div`
+//     border-top: 1px solid #ccc;
+//     margin-top: 20px;
+//     padding: 10px 0;
     
-    > p > span {
-        cursor: pointer;
-        text-decoration: underline;
-        color: red;
-    }
+//     > p > span {
+//         cursor: pointer;
+//         text-decoration: underline;
+//         color: red;
+//     }
 
-    > p:nth-of-type(3) {
-        font-size: 12px;
-        margin-top: 5px;
-    }
-`
+//     > p:nth-of-type(3) {
+//         font-size: 12px;
+//         margin-top: 5px;
+//     }
+// `
